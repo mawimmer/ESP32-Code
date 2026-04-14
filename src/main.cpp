c53adb1acddc857d38b8e6248b9544fbd7e95b89
Matt <126898280+mawimmer@users.noreply.github.com>
2026-04-14 15:15:48 +0200

    Setup for terminal debugging use

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-// Beim Nano ESP32 ist die LED oft an einem speziellen Pin
 void setup() {
-  Serial.begin(115200);
+    //Serial0 for reading content in VSC Terminal
+  Serial0.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); 
 }
 
 void loop() {
-  Serial.println("Der ESP32 lebt! Signal an Kessel...");
+  Serial0.println("ESP32 is alive!");
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
 }
+
