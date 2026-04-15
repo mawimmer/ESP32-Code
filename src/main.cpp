c1b011ecd876f00769ab12dad92b44597dc67bf7
Matt <126898280+mawimmer@users.noreply.github.com>
2026-04-15 18:08:00 +0200

    started defining helper function

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,28 @@
 #include <Arduino.h>
 #include <driver/pcnt.h>
 
-#define CLK 2
-#define DT 3
-#define SW 4
-
+#define PCNT_LIMIT_HIGH 21
+#define PCNT_LIMIT_LOW -21
 
 unsigned long lastBlinkTime = 0;
 bool ledState = LOW;
 
 
+void setup_PCNT_UNIT(pcnt_unit_t unit, int pin_clk, int pin_dt){
+    pcnt_config_t pcnt_config = {
+        .counter_h_lim = PCNT_LIMIT_HIGH,
+        .counter_l_lim = PCNT_LIMIT_LOW,
+        
+
+    };
+}
+
 
 void setup() {
 //Serial0 for reading content in VSC Terminal
     Serial0.begin(115200);
+    Serial0.println(String("ESP-IDF Version is: ") + esp_get_idf_version());
     pinMode(LED_BUILTIN, OUTPUT); 
-//Rotary Encoder Block - Start
-    //Pin declarations
-    pinMode(CLK, INPUT);
-    pinMode(DT, INPUT);
-    pinMode(SW, INPUT_PULLUP);
-    //PCNT Initiation
-//Rotary Encoder Block - End
-
-
 }
 
 void loop() {
