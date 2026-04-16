35ef46e5273f43171e2b855deedf59f27815c701
mawimmer <126898280+mawimmer@users.noreply.github.com>
2026-04-16 23:38:58 +0200

    Add multi-encoder support and update diagram for 2 simulated encoders

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,47 @@
 #include <Arduino.h>
 #include <driver/pcnt.h>
 
+//Serial0 for reading content in VSC Terminal -- needs to be removed for real hardware
+#define Serial Serial0
+
 #define PCNT_LIMIT_HIGH 21
 #define PCNT_LIMIT_LOW -21
 
-const int CLK = 5;
-const int DT = 6;
-const int SW = 7;
+// const int CLK = 5;
+// const int DT = 6;
+// const int SW = 7;
+
+const int NUM_ENCODERS = 4;
 
 unsigned long lastBlinkTime = 0;
 bool ledState = LOW;
 
 
+//Rotary Encoder Declarations
+
+
+
+enum Rotary_Encoder_MODI {
+    BRIGHTNESS_MODI,
+    EFFECT_MODI
+};
+
+//Struct holding the information of each unit defined
+struct RotaryEncoder{
+    //Hardware Info
+    pcnt_unit_t unit;
+    int pin_clk;
+    int pin_dt;
+    int pin_sw;
+
+    //Memory
+    int16_t lastValue;
+    //State Machine
+    // unsigned long TimeOfLastClick;
+
+    // unsigned long TimeOfLastRotation;
+    // bool confirmed = false;
+};
 
 
 /**
@@ -50,19 +80,50 @@ void setup_PCNT_UNIT(pcnt_unit_t unit, int pin_clk, int pin_dt){
 
 };
 
+RotaryEncoder Encoders[NUM_ENCODERS]{
+    {PCNT_UNIT_0, 5, 6, 7, 0},
+    {PCNT_UNIT_1, 8, 9, 10, 0},
+    {PCNT_UNIT_2, 17, 18, 21, 0},
+    {PCNT_UNIT_3, 1, 2, 3, 0}
+
+};
+
 
 void setup() {
-//Serial0 for reading content in VSC Terminal
-    Serial0.begin(115200);
-    Serial0.println(String("ESP-IDF Version is: ") + esp_get_idf_version());
+    Serial.begin(115200);
+    Serial.println(String("ESP-IDF Version is: ") + esp_get_idf_version());
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(CLK, INPUT_PULLUP);
-    pinMode(DT, INPUT_PULLUP);
-    pinMode(SW, INPUT_PULLUP);
-    setup_PCNT_UNIT(PCNT_UNIT_0, CLK, DT);
+    // pinMode(CLK, INPUT_PULLUP);
+    // pinMode(DT, INPUT_PULLUP);
+    // pinMode(SW, INPUT_PULLUP);
+    for (int i = 0 ; i < NUM_ENCODERS ; i++) {
+        pinMode(Encoders[i].pin_clk, INPUT_PULLUP);
+        pinMode(Encoders[i].pin_dt, INPUT_PULLUP);
+        pinMode(Encoders[i].pin_sw, INPUT_PULLUP);
+
+        setup_PCNT_UNIT(Encoders[i].unit, Encoders[i].pin_clk, Encoders[i].pin_dt );
+
+        Serial.printf("Encoder # %d has been initialized!", i );
+    };
 };
 
 void loop() {
+
+    for (int i = 0 ; i < NUM_ENCODERS; i++) {
+
+        int16_t ValueNOW;
+
+        pcnt_get_counter_value(Encoders[i].unit, &ValueNOW);
+
+        if(Encoders[i].lastValue != ValueNOW) {
+            Serial.printf("Encoder %d has a new Value: %d", i, ValueNOW);
+            Encoders[i].lastValue = ValueNOW;
+        };
+
+    };
+
+
+
     static bool initialized = false;
 
     if (millis() - lastBlinkTime >= 500) {
@@ -70,21 +131,12 @@ void loop() {
         lastBlinkTime = millis();
         ledState = !ledState;
         digitalWrite(LED_BUILTIN, ledState);
-    
-        if (ledState == HIGH) {
 
-            int16_t countValue;
-            int16_t* counterpointer = &countValue;
-
-            if(!initialized){
-                Serial0.println("ESP32 is alive!");
-                pcnt_counter_clear(PCNT_UNIT_0);
-                initialized = true;
-            };
-
-            pcnt_get_counter_value(PCNT_UNIT_0, counterpointer);
-            Serial0.println(countValue);
+        if(!initialized){
+            Serial.println("ESP32 is alive!");
+            initialized = true;
         };
+    
     };
 
 
